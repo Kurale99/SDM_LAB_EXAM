6c687303fe9ee172da46ae75cb656331ab359b9d
Sahil <sahilkurale9899@gmail.com>
2023-02-02 14:52:17 +0530

    first commit

---
+++ LinkedList.java
@@ -0,0 +1,43 @@
+
+class Node {
+    Node next;
+    Node head;
+    int data;
+
+    Node() {
+
+    }
+
+    Node(int data) {
+        this.data = data;
+        head = next = null;
+    }
+
+    public void insert(int data) {
+        Node new_node = new Node(data);
+
+        if (head == null) {
+            head = new_node;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = new_node;
+        new_node.next = null;
+    }
+}
+
+public class LinkedList {
+    public static void main(String[] args) {
+        Node n1 = new Node();
+        n1.insert(10);
+        n1.insert(20);
+        n1.insert(30);
+        n1.insert(40);
+        n1.insert(50);
+
+    }
+
+}
